17e0f5c4b9a2303c3dbb9e3ffbab53a23f145675
Victor <hoangtrandai@protonmail.com>
2022-01-21 12:01:52 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Victor Hoang on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
